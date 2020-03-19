e62a8b0d5585d09ddab3470476a41ab3bd57a3cb
Saadat-MBP16 <me@saadat.dev>
2020-03-19 22:42:24 +0100

    Update Maintenance => Include a VERY DANGEROUS eval command (thanks to the person at Git for the base code!) --> Locked to Bot creator for now (My DiscordID)
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- Cogs/Maintenance.py
+++ Cogs/Maintenance.py
@@ -1,3 +1,4 @@
+import ast
 import discord
 from discord.ext import commands
 
@@ -5,6 +6,40 @@ from discord.ext import commands
 class Maintenance(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    def insert_returns(self, body):
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+        if isinstance(body[-1], ast.If):
+            self.insert_returns(body[-1].body)
+            self.insert_returns(body[-1].orelse)
+        if isinstance(body[-1], ast.With):
+            self.insert_returns(body[-1].body)
+
+    @commands.command(name='eval')
+    async def eval_function(self, ctx, *, fnstr):
+        # restrict eval to the bot creator
+        if ctx.author.id != 485880883119783956:
+            return
+
+        fn_name = "_eval_expr"
+        cmd = fnstr.strip("` ")
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+        body = f"async def {fn_name}():\n{cmd}"
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+        self.insert_returns(body)
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__
+        }
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+        result = (await eval(f"{fn_name}()", env))
+        await ctx.send(result)
 
 
 def setup(bot):
