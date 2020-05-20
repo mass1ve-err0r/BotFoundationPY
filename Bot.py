c7f5a009d497a754b711523ee1932d59c5794e97
Saadat-MBP16 <me@saadat.dev>
2020-05-20 11:17:52 +0200

    Update Bot - can now make use of dotenv - Preparation for Music playback - Inclusion of Admin & APIFun Cogs --> Some functionality has been split up into their own Cogs, commits coming!
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- Bot.py
+++ Bot.py
@@ -3,17 +3,24 @@ from discord import Game
 from datetime import datetime
 from discord.ext import commands
 
+# dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
 bot = commands.Bot(command_prefix='?')
 bot.remove_command("help")
 
 # Disabled Cogs: AntiSpam, Logger, Filter, OptionalFilters and Music
-extensions = ['Cogs.Moderators',
+extensions = ['Cogs.Administrators',
+              'Cogs.Moderators',
+              'Cogs.APIFun',
               'Cogs.Fun',
               'Cogs.Maintenance']
 
 if __name__ == '__main__':
     for extension in extensions:
         bot.load_extension(extension)
+        print("[+]: Added " + extension)
 
 
 @bot.event
@@ -21,7 +28,8 @@ async def on_ready():
     await bot.wait_until_ready()
     print('Logged in at: {0}'.format(datetime.now()))
     print('We have logged in as {0.user}'.format(bot))
-    await bot.change_presence(activity=Game(name="Hey there, I'm Siri !"))
+    # bot.load_extension('Cogs.Music')  # we need a late init for this!
+    await bot.change_presence(activity=Game(name="nothing, but nice to meet you! I'm Siri !"))
 
 
 @bot.event
@@ -35,6 +43,7 @@ async def on_message(message):
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
+        print("[-]: Command not found ?")
         return
 
 bot.run(os.environ.get('TOKEN'))
